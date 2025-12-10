69f420cd619da0062e62d9e62fcec2e0959e9574
tetawowe <tetawowe.solfware@gmail.com>
2025-12-10 16:13:07 +0800

    Delete header/header.js

--- header/header.js
+++
@@ -1,8 +0,0 @@
-const headerContainer = document.getElementById('header-placeholder');
-
-fetch('/header/header.html')
-  .then(response => response.text())
-  .then(data => {
-    headerContainer.innerHTML = data;
-  })
-  .catch(err => console.error('Failed to load header.html:', err));
