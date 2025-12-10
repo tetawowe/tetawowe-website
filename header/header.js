6c112a3058cd6c2870d4e91cb33be28072d69572
tetawowe <tetawowe.solfware@gmail.com>
2025-12-10 16:50:09 +0800

    Add files via upload

---
+++ header/header.js
@@ -0,0 +1,8 @@
+const headerContainer = document.getElementById('header-placeholder');
+
+fetch('/header/header.html')
+  .then(response => response.text())
+  .then(data => {
+    headerContainer.innerHTML = data;
+  })
+  .catch(err => console.error('Failed to load header.html:', err));
