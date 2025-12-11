12229b054f9098e15a59d196251e2a2acfee283f
tetawowe <tetawowe.solfware@gmail.com>
2025-12-11 11:45:19 +0800

    Update header.js

--- header/header.js
+++ header/header.js
@@ -1,11 +1,12 @@
 const headerContainer = document.getElementById('header-placeholder');
 
-fetch('/tetawowe-website/header/header.html', { cache: "no-cache" })
+fetch('/header/header.html', { cache: "no-cache" })
   .then(response => response.text())
   .then(data => {
     headerContainer.innerHTML = data;
   })
   .catch(err => console.error('Failed to load header.html:', err));
+
 
 
 
